eed1877c3f61183ed189ae415d191a01db91557c
Gabriel Arjones <arjones@arjones.com>
2018-07-26 11:39:29 -0300

    disable collapsed prompt in fish

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 set fish_greeting ""
+set -U fish_prompt_pwd_dir_length 0
 set -g -x AUTOPROJ_OS "ubuntu,debian:16.04,xenial,xerus"
 
 source /usr/share/pantheon-terminal/enable-fish-completion-notifications 2>/dev/null; or true
